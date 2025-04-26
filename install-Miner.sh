28594d8ed355af5843b66f3c9224a28548fbacd9
Sardinha CN <50783766+SardinhaCN@users.noreply.github.com>
2025-04-26 15:37:21 -0300

    Update install-Miner.sh

--- install-Miner.sh
+++ install-Miner.sh
@@ -6,5 +6,4 @@ pkg install proot -y
 pkg install cmake -y
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build 
-cmake .. -DWITH_HWLOC=OFF && make 
-./xmrig -o rx.unmineable.com:3333 -u DOGE:YourAddress.MinerWorker -p x -k -a rx/0 -t 5
+cmake .. -DWITH_HWLOC=OFF && make
