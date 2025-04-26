83afbbfbab2e726130960b324c83ea5927e55e35
Sardinha CN <50783766+SardinhaCN@users.noreply.github.com>
2025-04-26 15:51:23 -0300

    Update main.sh

--- MenuMiner/main.sh
+++ MenuMiner/main.sh
@@ -2,7 +2,7 @@
 PASTA="./xmrig/build"
 COMANDOS_DIR="./comandos"
 mostrar_menu() {
-    echo "Escolha uma opção:"
+echo "Escolha uma opção:"
     # Lista os arquivos no diretório de comandos
     i=1
     comandos=()
